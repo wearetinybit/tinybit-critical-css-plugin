293f97ccc85ffee06a5da97f73484ea5fc48b706
Daniel Bachhuber <daniel@bachhuber.co>
2021-11-16 04:26:00 -0800

    Initial commit from `wp scaffold plugin`

---
+++ tinybit-critical-css.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Tinybit Critical Css
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     tinybit-critical-css
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Tinybit_Critical_Css
+ */
+
+// Your code starts here.
