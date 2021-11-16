99e6ddf4adbfe5191a1c50689678b9ed67765e97
Daniel Bachhuber <daniel@bachhuber.co>
2021-11-16 08:28:50 -0800

    Add some more config files

---
+++ tests/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package TinyBit_Critical_Css
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/tinybit-critical-css.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
+
+echo PHP_EOL;
+echo 'WP version: ' . $GLOBALS['wp_version'];
+echo PHP_EOL . PHP_EOL;
