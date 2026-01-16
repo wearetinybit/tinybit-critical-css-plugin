db366295eb0b3d25a0503e4171171363d3d0ccdd
jeremyfelt <jeremy.felt@gmail.com>
2026-01-16 09:47:04 -0800

    Bump the real version to 0.1.3

--- tinybit-critical-css.php
+++ tinybit-critical-css.php
@@ -7,7 +7,7 @@
  * Author URI:      https://tinybit.com
  * Text Domain:     tinybit-critical-css
  * Domain Path:     /languages
- * Version:         0.1.2
+ * Version:         0.1.3
  *
  * @package         Tinybit_Critical_Css
  */
