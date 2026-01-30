4a68fb5f7c47aa30c7b1ab6db3da500fb5dd1989
jeremyfelt <jeremy.felt@gmail.com>
2026-01-29 16:33:31 -0800

    Bump 0.1.5

--- tinybit-critical-css.php
+++ tinybit-critical-css.php
@@ -7,7 +7,7 @@
  * Author URI:      https://tinybit.com
  * Text Domain:     tinybit-critical-css
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  *
  * @package         Tinybit_Critical_Css
  */
